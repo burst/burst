60e84b22a7a6aa6cf4f77b43ff8964a6403e7e3a
unknown <mk@.el-vision.com>
2009-03-20 23:33:26 +0200

    First draft, functions doing what they are suposed do in the most simplistic way possible, but no accuracy whatsoever. Mind usage of AlMemory to store our variables. Testing script is testLocomotion.

---
+++ users/vova/testLocomotion.py
@@ -0,0 +1,13 @@
+""" Test our brand new Locomotion class
+"""
+
+from walkAPI import Locomotion
+
+c = Locomotion("127.0.0.1",9111)
+c.memoryProxy.insertData("Burst/Odometry/X",0.5,0)
+c.memoryProxy.insertData("Burst/Odometry/Y",0,0)
+c.memoryProxy.insertData("Burst/Odometry/Yaw",-1.57009,0)
+
+#c.testWalk()
+c.goToLocation(1,1,0)
+print "aaaa"
