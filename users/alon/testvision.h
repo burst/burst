6b15256ef1d12834054710d82723400d2b8e8271
Alon Levy <alonlevy1@gmail.com>
2009-04-02 01:40:21 +0300

    get a single remote image minimal example

---
+++ users/alon/testvision.h
@@ -0,0 +1,18 @@
+/**
+ * @author Patrick de Pas
+ * Aldebaran Robotics (c) 2007 All Rights Reserved - This is an example of use.\n
+ *
+ */
+
+#ifndef VISIONMODULE_H
+# define VISIONMODULE_H
+
+// ..::: Headers ::
+# include "alxplatform.h"
+# include <fstream>
+# include <sstream>
+# include <albroker.h>
+# include <almodule.h>
+# include <altools.h>
+
+#endif // VISIONMODULE_H
