fad509d475e5058c053f4a1d5c812254750c9b37
alon.levy <alon@amber.saymoo.org>
2009-04-16 01:28:10 +0300

    rewrite clutter client, show both images, like webots

--- users/alon/clutterclient.py
+++ users/alon/clutterclient.py
@@ -38,49 +38,57 @@ class Main(object):
         con.exploreToGetModuleByName('NaoCam')
         con.registerToCamera()
         self._c = 0
+        self._images = [None, None]
+        self._initGui()
 
-    def getImage(self):
-        con = self._con
-        meths = (lambda: (con.switchToBottomCamera(), con.getImageRemoteRGB()),
-             lambda: (con.switchToTopCamera(), con.getImageRemoteRGB()))
-        im = meths[self._c]()
+    def _initGui(self):
+        stage_color = clutter.Color(0x99, 0xcc, 0xff, 0xff)
+        
+        stage = clutter.Stage()
+        stage.connect('button-press-event', clutter.main_quit)
+        stage.connect('destroy', clutter.main_quit)
+        stage.set_color(stage_color)
+        stage.set_user_resizable(True)
+        stage.set_size(400, 200)
+        
+        self._textures = []
+        for i in range(2):
+            texture = clutter.Texture('top.jpg')
+            #texture.set_size(300, 300)
+            texture.set_position(i*200, 0)
+            #texture.set_from_file('top.jpg')
+            stage.add(texture)
+            #import pdb; pdb.set_trace()
+            self._textures.append(texture)
+            
+        #tick(texture)
+        gobject.timeout_add_seconds(1, self.onUpdateImage)
+        stage.show()
+        stage.show_all()
+        self.stage = stage
+ 
+    def getBottomImage(self):
+        self._con.switchToBottomCamera()
+        self._images[0] = self._con.getImageRemoteRGB()
+
+    def getTopImage(self):
+        self._con.switchToTopCamera()
+        self._images[1] = self._con.getImageRemoteRGB()
+
+    def onUpdateImage(self):
+        i = self._c
         self._c = 1 - self._c
-        return im[1]
+        texture = self._textures[i]
+        [self.getBottomImage, self.getTopImage][i]()
+        im, width, height = self._images[i]
+        print "%s: %s, %s" % (time() - start_time, width, height)
+        texture.set_from_rgb_data(im, False, width, height, width*3, 3, 0)
+        return True # required by gobject.timeout
 
 start_time = time()
 
-def tick(texture):
-    im, width, height = main.getImage()
-    print "%s: %s, %s" % (time() - start_time, width, height)
-    #import pdb; pdb.set_trace()
-    # second: has_alpha_channel (False means no, just RGB)
-    # last three: row stride in bytes, BytesPerPixel, Flags.
-    texture.set_from_rgb_data(im, False, width, height, width*3, 3, 0)
-    return True
-
 if __name__ == '__main__':
-    stage_color = clutter.Color(0x99, 0xcc, 0xff, 0xff)
-    
-    stage = clutter.Stage()
-    stage.connect('button-press-event', clutter.main_quit)
-    stage.connect('destroy', clutter.main_quit)
-    stage.set_color(stage_color)
-    stage.set_user_resizable(True)
-    stage.set_size(200, 200)
-    
-    texture = clutter.Texture('top.jpg')
-    #texture.set_size(300, 300)
-    texture.set_position(0, 0)
-    #texture.set_from_file('top.jpg')
-    stage.add(texture)
-    texture.show()
-    #import pdb; pdb.set_trace()
-    
-    #tick(texture)
-    gobject.timeout_add_seconds(1, tick, texture)
-    stage.show()
-    stage.show_all()
-    
+   
     import sys
     url = "http://localhost:9560/"
     if len(sys.argv) > 1:
