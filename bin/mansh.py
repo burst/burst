79852f6c5792b74f5c22c6a731725f3d2a02ff74
Alon Levy <alonlevy1@gmail.com>
2009-04-21 09:18:53 +0000

    rewrite of mansh, supports readline, just history

--- bin/mansh.py
+++ bin/mansh.py
@@ -1,39 +1,58 @@
 #!/usr/bin/env python
 
-import burst
+import readline
 import cPickle
+import os
+
+import burst
+
+HISTORY_FILE=os.path.join(os.getenv('HOME'), '.mansh_history')
 
 # use --ip, --port to this shell
 
 def execer(txt):
     exec(txt)
 
-def main():
-    burst.init()
-    man=burst.ALProxy('Man')
+class Main:
+    def __init__(self):
+        if os.path.exists(HISTORY_FILE):
+            readline.read_history_file(HISTORY_FILE)
+        burst.init()
 
-    print man.getMethodList()
+    def main(self):
+        self.man = burst.ALProxy('Man')
 
-    man_func = man.pyEval
-    state = 'eval'
-    state_func = {'eval': man.pyEval, 'exec': man.pyExec, 'local': execer}
-    states = state_func.keys()
-    while True:
-        print "%s>>" % state,
-        cmd = raw_input()
-        if cmd in states:
-            man_func = state_func[cmd]
-            state = cmd
-            continue
-        res = man_func(cmd)
-        if state != 'eval':
-            continue
-        if res == 'error':
-            print "ERROR"
-        else:
-            last = cPickle.loads(res)
-            print last
+        print self.man.getMethodList()
+
+        self.man_func = self.man.pyEval
+        self.state = 'eval'
+        self.state_func = {'eval': self.man.pyEval,
+            'exec': self.man.pyExec, 'local': execer}
+        self.states = self.state_func.keys()
+        try:
+            self.mainloop()
+        except (KeyboardInterrupt, EOFError):
+            print "writing readline history file to %s" % HISTORY_FILE
+            readline.write_history_file(HISTORY_FILE)
+
+    def mainloop(self):
+        while True:
+            prompt = "%s>>" % self.state
+            cmd = raw_input(prompt)
+            if cmd in self.states:
+                self.man_func = self.state_func[cmd]
+                self.state = cmd
+                continue
+            res = self.man_func(cmd)
+            if self.state != 'eval':
+                continue
+            if res == 'error':
+                print "ERROR"
+            else:
+                last = cPickle.loads(res)
+                print last
+
 
 if __name__ == '__main__':
-    main()
+    Main().main()
 
