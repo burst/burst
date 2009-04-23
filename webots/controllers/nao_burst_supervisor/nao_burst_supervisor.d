40415b4b6887dfe34132596ed499fee1ebc37e18
Eran Polosetski <eranpolo@gmail.com>
2009-04-23 16:15:05 +0000

    webots worlds and controllers, adding some binary files

---
+++ webots/controllers/nao_burst_supervisor/nao_burst_supervisor.d
@@ -0,0 +1,7 @@
+nao_burst_supervisor.o: nao_burst_supervisor.c RoboCupGameControlData.h \
+  /usr/local/webots/include/controller/c/webots/robot.h \
+  /usr/local/webots/include/controller/c/webots/types.h \
+  /usr/local/webots/include/controller/c/webots/supervisor.h \
+  /usr/local/webots/include/controller/c/webots/nodes.h \
+  /usr/local/webots/include/controller/c/webots/emitter.h \
+  /usr/local/webots/include/controller/c/webots/receiver.h
