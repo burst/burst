deee8ed7827c69c96ce0e68d2788fc2dba4c2abe
Elad Alon <elad.alon@gmail.com>
2009-05-19 00:25:22 +0300

    As promised, WalkParameters now supports *getting* the parameters in either degrees or radians.

--- lib/burst/walkparameters.py
+++ lib/burst/walkparameters.py
@@ -15,17 +15,27 @@ class WalkParameters(list):
             super(WalkParameters, self).__init__(*args, **kwargs)
         else:
             super(WalkParameters, self).__init__([0.0 for item in xrange(15)])
+
         for var in ['ShoulderMedian', 'ShoulderAmplitude', 'ElbowMedian', 'ElbowAmplitude']:
+
             def f(value):
                 self[getattr(self, var)] = value * DEG_TO_RAD
             setattr(self, "set"+var+"Deg", f)
+            setattr(self, "get"+var+"Deg", lambda: self[getattr(self, var)] * RAD_TO_DEG)
+
             def f(value):
                 self[getattr(self, var)] = value
             setattr(self, "set"+var+"Rad", f)
+            setattr(self, "get"+var+"Rad", lambda: self[getattr(self, var)])
+
         for var in ['LHipRoll', 'RHipRoll', 'TorsoYOrientation']:
+
             def f(value):
                 self[getattr(self, var)] = value
             setattr(self, "set"+var+"Deg", f)
+            setattr(self, "get"+var+"Deg", lambda: self[getattr(self, var)])
+
             def f(value):
                 self[getattr(self, var)] = value * RAD_TO_DEG
             setattr(self, "set"+var+"Rad", f)
+            setattr(self, "get"+var+"Rad", lambda: self[getattr(self, var)] * DEG_TO_RAD)
