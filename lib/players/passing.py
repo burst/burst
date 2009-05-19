014414146396269280f0eb3cf2ee58cfc9dec242
burst <burst@maverick-desktop.(none)>
2009-05-19 18:36:57 +0300

    added initial passing file for projectants

---
+++ lib/players/passing.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# import player_init MUST BE THE FIRST LINE
+import player_init
+
+from burst.events import *
+from burst.consts import *
+from burst.player import Player
+from burst.events import *
+from burst.consts import *
+import burst.actions as actions
+import burst.moves as moves
+from burst.world import World
+from math import cos, sin
+
+
+def pr(s):
+    print s
+
+class Passing(Player):
+
+    def onStart(self):
+        # go to init position
+        self._actions.initPoseAndStiffness()
+
+        # Kick with left
+        print "Left kick!"
+        self._actions.kick(actions.KICK_TYPE_STRAIGHT_WITH_LEFT).onDone(doQuit)
+
+    def doQuit(self):
+        self._eventmanager.quit()
+
+if __name__ == '__main__':
+    import burst
+    from burst.eventmanager import MainLoop
+    MainLoop(Passing).run()
+
