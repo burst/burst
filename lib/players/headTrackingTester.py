b279d459ddac06367302b59905339350231e8f2f
Eran Polosetski <eran@anat-laptop.(none)>
2009-05-27 03:43:22 +0300

    headTrackingTester: added printing of last ball location

--- lib/players/headTrackingTester.py
+++ lib/players/headTrackingTester.py
@@ -18,8 +18,13 @@ class headTrackingTester(Player):
         self._actions.initPoseAndStiffness()
         self._actions.executeSyncHeadMove(moves.HEAD_MOVE_FRONT_BOTTOM)
         self._eventmanager.register(EVENT_BALL_IN_FRAME, self.trackBall)
+        self._last_ball_loc = (0.0, 0.0)
     
     def trackBall(self):
+        new_ball_loc = self._world.ball.centerX, self._world.ball.centerY
+        if new_ball_loc != self._last_ball_loc:
+            print "ball at %s" % str(new_ball_loc)
+            self._last_ball_loc = new_ball_loc
         self._actions.executeTracking(self._world.ball)
         #print "Ball seen!: (ball seen %s, dist: %3.3f, distSmoothed: %3.3f, ball bearing: %3.3f)" % (self._world.ball.seen, self._world.ball.dist, self._world.ball.distSmoothed, self._world.ball.bearing)
         #(ball_x, ball_y) = polar2cart(self._world.ball.distSmoothed, self._world.ball.bearing)
