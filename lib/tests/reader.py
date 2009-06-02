5ddee65920fb4c456cf69f93c84ad167432613e2
Eran Polosetski <eranpolo@gmail.com>
2009-06-02 17:00:01 +0000

    better burstmem tester

--- lib/tests/reader.py
+++ lib/tests/reader.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 
+"""
+Test the memory mapped file used in liue of ALProxy("ALMemory").
+
+to use:
+first run any of the players, i.e. players/donothing.py
+then run this with two vars, the first index to display and the last.
+i.e., for ball:
+./reader.py 26 84
+"""
+
 import os
 import mmap
 import struct
-
+import time
 import sys
 sys.path.append('..')
 import burst
@@ -19,31 +29,32 @@ MAX_INT = 1<<32
 
 def get_var_names():
     shm = burst.ALProxy(BURST_SHARED_MEMORY_PROXY_NAME)
+    shm = get_burst()
     # TODO - this code only works when not getting deferreds.
     num_vars = shm.getNumberOfVariables()
     return [shm.getVarNameByIndex(i) for i in xrange(num_vars)]
 
-def main():
-    #fd = os.open(filename, os.O_RDONLY)
+def display_vars(indices):
     data = open(filename, 'r')
     fd = data.fileno()
     buf = mmap.mmap(fd, totalsize, mmap.MAP_SHARED | mmap.ACCESS_READ, mmap.PROT_READ)
+    while True:
+        vars = [struct.unpack('f', buf[i*4:i*4+4])[0] for i in indices]
+        print vars
+        time.sleep(0.5)
+
+def main():
+    #fd = os.open(filename, os.O_RDONLY)
 
     import sys
     vars = None
+    display_vars(range(int(sys.argv[1]), int(sys.argv[2])))
+    return
     if len(sys.argv) > 1:
         vars = sys.argv[1:]
         total_vars = get_var_names()
         indices = [vars.index(var) for var in total_vars]
         print "indices = %s" % str(indices)
-    while True:
-        if vars:
-            values = [struct.unpack('f', buf[i*4:i*4+4])[0] for i in indices]
-            print ' '.join('%10s' % ('%3.3f' % v) for v in values)
-        else:
-            thesum = sum(struct.unpack('i', buf[i:i+4])[0] for i in xrange(0, totalsize, 4))
-            print thesum
 
 if __name__ == '__main__':
     main()
-
