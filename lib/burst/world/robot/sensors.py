22d57482dff18db67881fcfb518e636fe13a7276
Elad Alon <elad.alon@gmail.com>
2009-06-18 01:50:23 +0300

    Major cleanup of sensors.py. It's been bothering me. Done now.

--- lib/burst/world/robot/sensors.py
+++ lib/burst/world/robot/sensors.py
@@ -10,28 +10,29 @@ __all__ = ['Sensors']
 class Sensors(object):
 
     class Sensor(object):
+        def __init__(self, world, var):
+            self._world = world
+            self._var = var
+            world.addMemoryVars([var])
         def read(self):
-            pass
+            return self._world.vars[self._var]
         def update(self):
             pass
 
     class FSR(Sensor):
-        _var = 'Device/SubDeviceList/%sFoot/FSR/%s/Sensor/Value'
         def __init__(self, world, foot, location):
-            self._world = world
-            self._foot = foot
-            self._location = location
-            world.addMemoryVars( [Sensors.FSR._var % (self._foot, self._location)] )
-        def read(self):
-            return self._world.vars[Sensors.FSR._var % (self._foot, self._location)]
+            var = 'Device/SubDeviceList/%sFoot/FSR/%s/Sensor/Value' % (foot, location)
+            super(Sensors.FSR, self).__init__(world, var)
 
     class FootFsrCluster(Sensor):
         def __init__(self, world, foot):
             self._fsrs = []
             for location in [x+y for x in ['Front', 'Rear'] for y in ['Left', 'Right']]:
                 self._fsrs.append( Sensors.FSR(world, foot, location) )
+        def read(self):
+            return min(map(lambda x: x.read(), self._fsrs)) 
         def isPressed(self):
-            return min(map(lambda x: x.read(), self._fsrs)) < 1000
+            return self.read() < 1000
 
     class RightFootFSRs(FootFsrCluster):
         def __init__(self, world):
@@ -43,22 +44,15 @@ class Sensors(object):
 
     class IntertialSensor(Sensor): # Note: GyrRef, GyrX, AccX...
         def __init__(self, world, var):
-            self._world = world
-            self._var = var
-            self._world.addMemoryVars([self._var])
-        def read(self):
-            return self._world.vars[self._var]
+            super(Sensors.IntertialSensor, self).__init__(world, var)
 
     class YAngleIntertialSensor(IntertialSensor):
         def __init__(self, world):
-            super(Sensors.YAngleIntertialSensor, self).__init__(
-                world, "Device/SubDeviceList/InertialSensor/AngleY/Sensor/Value"
-            )
-        def update(self):
-            pass
+            var = "Device/SubDeviceList/InertialSensor/AngleY/Sensor/Value"
+            super(Sensors.YAngleIntertialSensor, self).__init__(world, var)
 
     def __init__(self, world):
-        self.debug = 0
+#        self.debug = 0
         self.rightFootFSRs = Sensors.RightFootFSRs(world)
         self.leftFootFSRs = Sensors.LeftFootFSRs(world)
         self.yAngleIntertialSensor = Sensors.YAngleIntertialSensor(world)
@@ -92,6 +86,6 @@ class Sensors(object):
         if self.isOnBelly():
             events.add(events_module.EVENT_FALLEN_DOWN)
             events.add(events_module.EVENT_ON_BELLY)
-#        self.debug += 1
+        self.debug += 1
 #        print self.debug, events, self.rightFootFSRs.isPressed(), self.leftFootFSRs.isPressed(), self.yAngleIntertialSensor.read()
 #        print map(lambda x: x.read(), self.rightFootFSRs._fsrs), map(lambda x: x.read(), self.leftFootFSRs._fsrs)
