4086b3d9771b8e8278729446edb56e85f8537d39
Asaf Shiloni <asafshiloni@gmail.com>
2009-06-22 20:32:22 +0300

    as god said, thay shall build a new test for every different case, 'cause I am Alon - I mean tha lord

--- lib/burst/personal/maldini.py
+++ lib/burst/personal/maldini.py
@@ -1,6 +1,29 @@
 """ Personal file for a robot by the name of this file. """
-from burst.moves.walks import STRAIGHT_WALK
-STRAIGHT_WALK.defaultSpeed = 80
+from .. import walkparameters; WalkParameters = walkparameters.WalkParameters
+from burst.moves.walks import walks
+from burst_consts import DEG_TO_RAD
+
+walks.FIRST_TWO_SLOW_STEPS = False
+walks.STRAIGHT_WALK = walks.Walk(WalkParameters([
+           100.0 * DEG_TO_RAD, # ShoulderMedian
+           20.0 * DEG_TO_RAD,  # ShoulderAmplitude
+           30.0 * DEG_TO_RAD,  # ElbowMedian 
+           20.0 * DEG_TO_RAD,  # ElbowAmplitude 
+           5,                   # LHipRoll(degrees) 
+           -5,                  # RHipRoll(degrees)
+           0.19,                  # HipHeight(meters)
+           -4.0,                   # TorsoYOrientation(degrees) - stopped adjusting to the negative direction - there is a possibility that a little bit more negative is better
+           0.055,                  # StepLength
+           0.015,                  # StepHeight
+           0.02,                  # StepSide
+           0.3,                   # MaxTurn
+           0.013,                  # ZmpOffsetX
+           0.015]),                  # ZmpOffsetY
+           25          # 20ms count per step
+    )
+
+
+#STRAIGHT_WALK.defaultSpeed = 80
 
 import burst.behavior_params as params
 params.KICK_X_MIN = [14,14]
---
+++ lib/players/tests/straightWalkTester.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+# DON'T PUT ANYTHING BEFORE THIS LINE
+import player_init
+
+from burst.behavior import InitialBehavior
+from burst_consts import *
+import burst.moves.walks as walks
+import burst.moves.poses as poses
+
+import time
+
+class WalkTester(InitialBehavior):
+    
+    def __init__(self, actions):
+        InitialBehavior.__init__(self, actions=actions, name=self.__class__.__name__)
+
+    def _start(self, firstTime=False):
+        self.movesList = [
+#                     ('Walk straight - Asaf''s version (Stable walk)',
+#                      lambda: self._actions.executeMoveRadians(poses.STABLE_WALK_INITIAL_POSE).onDone(
+#                      lambda: self._actions.changeLocationRelative(50.0, 0.0, 0.0, walk=walks.STABLE_WALK))),
+
+
+                      ('Walk straight',
+                      lambda: self._actions.executeMoveRadians(poses.STRAIGHT_WALK_INITIAL_POSE).onDone(
+                      lambda: self._actions.changeLocationRelative(50.0, 0.0, 0.0, walk=walks.STRAIGHT_WALK))),
+                      
+#                      ('Walk arc',
+#                      lambda: self._actions.executeMoveRadians(poses.STRAIGHT_WALK_INITIAL_POSE).onDone(
+#                      lambda: self._actions.changeLocationArc(100.0, 100.0, walk=walks.STRAIGHT_WALK))),
+
+#                     ('Walk sideways',
+#                      lambda: self._actions.changeLocationRelativeSideways(0.0, 20.0, walk=walks.SIDESTEP_WALK)),
+
+#                     ('Turn 90 degrees (Straight walk params)',
+#                      lambda: self._actions.turn(1.57, walk=walks.STRAIGHT_WALK)),
+
+#                     ('Sit',
+#                     lambda: self._actions.sitPoseAndRelax())
+                     ]
+        
+        self.doNextMove()
+        
+    def doNextMove(self):
+        if len(self.movesList) > 0:
+            self.testMove(self.movesList.pop(0))
+        else:
+            self.onTestDone()
+    
+    def testMove(self, (moveName, moveFunc)):
+        print "%s Started!" % moveName
+        self.moveStartTime = time.time()
+        moveFunc().onDone(self.onMoveDone)
+        
+    def onMoveDone(self):
+        print "Took approximately %f" % (time.time() - self.moveStartTime)
+        self.doNextMove()
+    
+    def onTestDone(self):
+        self._eventmanager.quit()
+
+if __name__ == '__main__':
+    import burst
+    from burst.eventmanager import MainLoop
+    MainLoop(WalkTester).run()
