5c3af6efc980af3a255d47c3b8b09735048f7f1c
Asaf Shiloni <asafshiloni@gmail.com>
2009-06-24 01:42:09 +0300

    diagonal tester and diagonalTester

--- lib/players/tests/diagonalTester.py
+++ lib/players/tests/diagonalTester.py
@@ -6,32 +6,45 @@ from burst.behavior import InitialBehavior
 from burst_events import EVENT_BALL_IN_FRAME
 import burst.moves.poses as poses
 from burst_util import polar2cart
+from burst.actions.target_finder import TargetFinder
+from diagonalkicker import *
 
 class DiagonalTester(InitialBehavior):
     
     def __init__(self, actions):
         InitialBehavior.__init__(self, actions=actions, name=self.__class__.__name__)
-
+        self._ballFinder = TargetFinder(actions=actions, targets=[self._world.ball], start=False)
+        self._ballFinder.setOnTargetFoundCB(self.ball_found)
+        self._ballFinder.setOnTargetLostCB(self.ball_lost)
+        
     def _start(self, firstTime=False):
         self._actions.setCameraFrameRate(20)
         self._actions.executeHeadMove(poses.HEAD_MOVE_FRONT_FAR).onDone(self.find_goal)
     
     def find_goal(self):
-        self._actions.localize().onDone(self.foundGoal)
+        self._actions.localize().onDone(self.found_goal)
 
-    def foundGoal(self):
+    def found_goal(self):
         yglp, ygrp = self._world.yglp, self._world.ygrp
         t = self._world.time
         dtl, dtr = t - yglp.update_time, t - ygrp.update_time
         print "Goal Posts Bearings: %3.2f, %3.2f (seens %s, %s), (updated %3.2f seconds ago)" % (
                 yglp.bearing, ygrp.bearing, yglp.seen, ygrp.seen, max(dtl, dtr))
-        self._eventmanager.callLater(1.0, self.track)
-        #self._eventmanager.register(self.printBall, EVENT_BALL_IN_FRAME)
+        
+        self._actions.executeHeadMove(poses.HEAD_MOVE_FRONT_BOTTOM).onDone(
+            lambda: self._eventmanager.callLater(0.5, self._ballFinder.start))
 
-    def track(self):
-        def callTrackLater():
-            self._eventmanager.callLater(0.0, self.track)
-        self._actions.tracker.track(self._world.ball, lostCallback=callTrackLater)
+    def ball_found(self):
+        print "BALL FOUND"
+        (ball_x, ball_y) = polar2cart(self._world.ball.distSmoothed, self._world.ball.bearing)
+        (side, kick_parameter) = getKickingType(yglp.bearing, ball_y)
+        if side == None or kick_parameter == None:
+            print "ERROR: could not find the right kicking parameter"
+        else:
+            self._actions.adjusted_straight_kick(side , kick_parameter)
+        
+    def ball_lost(self):
+        print "BALL LOST"
 
     def printBall(self):
 
