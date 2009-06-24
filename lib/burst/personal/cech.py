2a9701556445c5c2161d5d0f56888c32d1939c57
Asaf Shiloni <asafshiloni@gmail.com>
2009-06-25 00:49:54 +0300

    cech is nao tuned :)

--- lib/burst/personal/cech.py
+++ lib/burst/personal/cech.py
@@ -4,8 +4,12 @@
 import burst.behavior_params as params
 params.KICK_X_MIN = [14.0, 14.0]
 params.KICK_X_MAX = [20.0, 20.0]
-params.KICK_Y_MIN = [2, -3.5]
-params.KICK_Y_MAX = [5, -6.5]
+#params.KICK_Y_MIN = [3.5, -4.5]
+#params.KICK_Y_MAX = [6.5, -7.5]
+params.KICK_Y_MIN = [3.65, -4.5]
+params.KICK_Y_MAX = [6.65, -7.5]
+
+
 
 ## Choreograph moves
 import burst.moves.choreograph as choreograph
