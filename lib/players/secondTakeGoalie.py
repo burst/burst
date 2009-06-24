4d55845001af6cd9d2db3dc8e64750bcbd80f534
Elad Alon <elad.alon@gmail.com>
2009-06-24 23:01:30 +0300

    The goalie has reached a milestone.

--- lib/players/secondTakeGoalie.py
+++ lib/players/secondTakeGoalie.py
@@ -40,6 +40,7 @@ def debugged(f):
 
 class Goalie(InitialBehavior):
 
+#    @debugged
     def __init__(self, actions):
         InitialBehavior.__init__(self, actions=actions, name=self.__class__.__name__, initial_pose=poses.SIT_POS)
         self.sideLeaptTo = left # TODO: Get this from the previous states, since you'll be incorporated into goalie.py.
@@ -50,7 +51,6 @@ class Goalie(InitialBehavior):
         print "_start"
         # On having gotten up, turn to the other side until you've found goal.
         # TODO: We might want to prop the head a little higher.
-#        self._actions.moveHead((pi/2)*(-1*self.sideLeaptTo), -40.0*DEG_TO_RAD).onDone(self.closedLoopFindOppositeOwnPost)
         self._actions.moveHead((pi/2)*(-1*self.sideLeaptTo), -40.0*DEG_TO_RAD).onDone(self.findOppositeOwnPost)
 
 #    @debugged
@@ -74,21 +74,31 @@ class Goalie(InitialBehavior):
     def onFocusedOnOppositeOwnPost(self, goalpost):
         print "onFocusedOnOppositeOwnPost"
         print "DISTANCE:", goalpost.dist
-        '''
         self._actions.executeMove(poses.STRAIGHT_WALK_INITIAL_POSE).onDone(
             lambda: self._actions.moveHead((pi/2)*(-1*self.sideLeaptTo), -40.0*DEG_TO_RAD).onDone(
             lambda: self._actions.changeLocationRelativeSideways(0.0, -self.sideLeaptTo*max(goalpost.dist-VOVA_OFFSET, 0.0)).onDone(
-            self.onVova)))
-        '''
-        self._actions.executeMove(poses.STRAIGHT_WALK_INITIAL_POSE).onDone(
-            lambda: self._actions.moveHead((pi/2)*(-1*self.sideLeaptTo), -40.0*DEG_TO_RAD).onDone(
-            lambda: self._actions.changeLocationRelativeSideways(0.0, -self.sideLeaptTo*max(goalpost.dist-VOVA_OFFSET, 0.0)).onDone(
-            self.onVova)))
+            lambda: self._actions.moveHead(0.0, -40.0*DEG_TO_RAD).onDone(self.onVova))))
 
 #    @debugged
     def onVova(self):
-        print "onVova"
-        self._eventmanager.quit()
+        print 'onVova'
+        self._eventmanager.register(self.alignmentAgainstOppositeGoalTester, EVENT_STEP)
+        self._actions.turn(self.sideLeaptTo*2*pi)
+
+#    @debugged
+    def alignmentAgainstOppositeGoalTester(self):
+#        print 'alignmentAgainstOppositeGoalTester'
+        seenOppositePosts = filter(lambda obj: obj.seen, [self._world.opposing_goal.unknown, self._world.opposing_lp, self._world.opposing_rp])
+        aligned = lambda obj: obj.centerX > burst_consts.IMAGE_CENTER_X if self.sideLeaptTo == left else obj.centerX < burst_consts.IMAGE_CENTER_X
+        if any(map(aligned, seenOppositePosts)):
+            self._eventmanager.unregister(self.alignmentAgainstOppositeGoalTester, EVENT_STEP)
+            self._actions.clearFootsteps().onDone(lambda e: self.onAligned())
+
+    def onAligned(self):
+        print "ALIGNED!"
+        # Intentionaly, no self._eventmanager.quit()
+
+
 
 #######################################################################################################################
 
