af2a768fcbec0d2cc4cc9089c687abebbf9768e0
Elad Alon <elad.alon@gmail.com>
2009-06-25 02:07:33 +0300

    Some more fixes for the merge of the goalie.

--- lib/burst/actions/goalie/alignment_after_leap.py
+++ lib/burst/actions/goalie/alignment_after_leap.py
@@ -3,7 +3,7 @@
 #######################################################################################################################
 
 import player_init
-from burst.behavior import InitialBehavior
+from burst.behavior import Behavior
 from burst_events import *
 from burst_consts import *
 import burst.moves.poses as poses
