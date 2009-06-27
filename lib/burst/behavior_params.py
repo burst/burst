605f5df9f1d06980166c377c59013297b0bcd82a
alon.levy <alon@amber.saymoo.org>
2009-06-27 16:09:30 +0300

    default kick set to None in getKickingType

--- lib/burst/behavior_params.py
+++ lib/burst/behavior_params.py
@@ -135,7 +135,7 @@ def getKickingType(self, goal_bearing, ball_y, side, margin=0):
     print "ball is at ", ball_y, ", delta is ", delta_y, ", so foot needs to be at ", foot_y
     
     # Alon: set default kick_parameter, since your if's didn't cover all bases.
-    kick_parameter = 0.5
+    kick_parameter = None
 
     if side == LEFT: 
         if foot_y >= KICK_Y_MIN[LEFT]-margin and foot_y <= KICK_Y_MAX[LEFT]+margin:
