74b5335810f78f21d4c00043115adc13df7fb8e5
Eran Polosetski <eran@anat-laptop.(none)>
2009-07-01 09:35:23 +0300

    some tests, nbfrm2png, and untested mergetables

---
+++ lib/players/tests/quitting_behavior.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import player_init
+
+from burst.behavior import InitialBehavior
+
+class Template(InitialBehavior):
+
+    def __init__(self, actions):
+        InitialBehavior.__init__(self, actions=actions, name=self.__class__.__name__)
+
+    def _start(self, firstTime=False):
+        self._eventmanager.callLater(1.0, self.stop)
+
+if __name__ == '__main__':
+    from burst.eventmanager import MainLoop
+    MainLoop(Template).run()
+
