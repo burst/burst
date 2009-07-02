5d286db5a80efda1c8884eb84931b70052f42e51
Roy Ben-Ari <roy@roy-laptop.(none)>
2009-07-02 11:44:02 +0300

    reduce ball_between_legs for Webots only

--- lib/burst/personal/webots.py
+++ lib/burst/personal/webots.py
@@ -4,7 +4,7 @@
 import burst.behavior_params as params
 params.KICK_X_MIN = [15.0,15.0] #[30.0,30.0]
 params.KICK_X_MAX = [21.0,21.0] #[33.0,33.0]
-params.KICK_Y_MIN = [4.0,-4.0] #[6.0,-6.0]
+params.KICK_Y_MIN = [3.0,-3.0] #[6.0,-6.0]
 params.KICK_Y_MAX = [10.0,-10.0] #[12.5,-12.5]
 params.MOVEMENT_PERCENTAGE_FORWARD = 0.7 # was 0.55, trying higher values
 params.MOVEMENT_PERCENTAGE_SIDEWAYS = 0.9
